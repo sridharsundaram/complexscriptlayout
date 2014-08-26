c056868d23e097a92aae58da7d007e053ceaeaeb
sridhar sundaram <sridhar.sundaram@gmail.com>
2014-08-26 12:22:27 +0530

    allowing 32 and 64 bit windows. 32 bit not tested

--- harfbuzz/jni/Win64.mk
+++ harfbuzz/jni/Win64.mk
@@ -5,8 +5,8 @@ LOCAL_STATIC_LIBRARIES := ft2 harfbuzz
 
 complex_script_layout: complex_script_layout_win.c
 	@rm -f complex_script_layout.dll
-	cd freetype; make -f Win.mk libft2; cd ..
-	cd harfbuzz; make -f Win.mk harfbuzz; cd ..
+	cd freetype; make -f Win64.mk libft2; cd ..
+	cd harfbuzz; make -f Win64.mk harfbuzz; cd ..
 	gcc -Wall -m64 -c -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -I"C:/Program Files/Java/jdk1.8.0_11/include" -I"C:/Program Files/Java/jdk1.8.0_11/include/win32" \
    -I"E:/Users/Sridhar/git/harfbuzz/harfbuzz/jni/harfbuzz/src" -I"E:/users/sridhar/git/harfbuzz/harfbuzz/jni/freetype/include"  complex_script_layout_win.c
 	g++ -m64 -Wl,-subsystem,windows -shared -o complex_script_layout64.dll freetype/*.o harfbuzz/*.o complex_script_layout_win.o
---
+++ harfbuzz/jni/harfbuzz/Win32.mk
@@ -0,0 +1,44 @@
+#
+#############################################################
+#   build the harfbuzz library
+#
+
+LOCAL_CFLAGS := -DHAVE_OT -O2 -DHB_NO_UNICODE_FUNCS -D__MINGW32__
+LOCAL_PATH:=E:/Users/sridhar/git/harfbuzz/harfbuzz/jni/harfbuzz
+
+LOCAL_SRC_FILES:= \
+        src/hb-blob.cc \
+        src/hb-buffer.cc \
+        src/hb-common.cc \
+        src/hb-fallback-shape.cc \
+        src/hb-font.cc \
+        src/hb-ft.cc \
+        src/hb-ot-layout.cc \
+        src/hb-ot-map.cc \
+        src/hb-ot-shape.cc \
+        src/hb-ot-shape-complex-arabic.cc \
+        src/hb-ot-shape-complex-indic.cc \
+        src/hb-ot-shape-complex-misc.cc \
+        src/hb-ot-shape-fallback.cc \
+        src/hb-ot-shape-normalize.cc \
+        src/hb-ot-tag.cc \
+        src/hb-set.cc \
+        src/hb-shape.cc \
+        src/hb-shaper.cc \
+        src/hb-shape-plan.cc \
+        src/hb-tt-font.cc \
+        src/hb-unicode.cc
+
+LOCAL_C_INCLUDES += \
+        -I"$(LOCAL_PATH)/src" \
+        -I"$(LOCAL_PATH)/../freetype/include"
+
+LOCAL_MODULE:= harfbuzz
+
+
+harfbuzz: $(LOCAL_SRC_FILES);
+	gcc -c -m32 -msse2 $(LOCAL_CFLAGS) $(LOCAL_C_INCLUDES) $(LOCAL_SRC_FILES)
+  
+LOCAL_STATIC_LIBRARIES:= ft2
+
+include $(BUILD_STATIC_LIBRARY)
